b04a5dfb802610a0044e6c4e8f92085acdd7f0fa
Monet Man <88108211+jpcisneros411@users.noreply.github.com>
2022-01-25 14:27:04 -0600

    make it better

---
+++ routes/apiRoutes.js
@@ -0,0 +1,30 @@
+const db = require("../models");
+
+module.exports = function (app) {
+
+  app.get("/api/workouts", (req, res) => {
+    db.Workout.find({}).then(dbWorkout => {
+      res.json(dbWorkout);
+    });
+  });
+
+  app.put("/api/workouts/:id", (req, res) => {
+    db.Workout.findByIdAndUpdate(req.params.id,
+      { $push: { exercises: req.body } }).then(dbWorkout => {
+        res.json(dbWorkout);
+      });
+  });
+
+  app.post("/api/workouts", (req, res) => {
+    db.Workout.create(req.body, (error, success) => {
+      if (error) throw error;
+      res.json(success);
+    });
+  });
+
+  app.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({}).limit(7).then(dbWorkout => {
+      res.json(dbWorkout);
+    });
+  });
+};
